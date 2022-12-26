ac9e493cd6c6adcd2f9237957319635a8be683e9
BadgerSIO <saadibneomar@gmail.com>
2022-12-26 19:31:18 +0600

    login form phone regex added

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 const Login = () => {
   return (
     <section className=" h-screen flex flex-col justify-center items-center">
-      <h1 className=" text-xl mb-5">Welcome to healthOS</h1>
+      <h1 className=" text-3xl z-10 -mb-3 font-titles">Welcome to healthOS</h1>
       <div className=" bg-white flex justify-center items-center ">
         <div className="bg-white/25 backdrop-blur-xl w-96 px-5 py-10 rounded border border-gray-200">
           <Outlet />
--- src/pages/Login/LoginForm.js
+++ src/pages/Login/LoginForm.js
@@ -1,21 +1,41 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const login = (data) => {
+    console.log(data);
+  };
   return (
     <div>
       <h1 className="text-xl font-semibold mb-5">Login</h1>
-      <form className="space-y-5">
+      <form className="space-y-5" onSubmit={handleSubmit(login)}>
         <div>
-          <label htmlFor="email" className="text-sm capitalize">
+          <label htmlFor="phoneNumber" className="text-sm capitalize">
             Phone number
           </label>
           <input
-            type="email"
-            name="email"
-            id="email"
+            type="tel"
+            {...register("phoneNumber", {
+              required: "This field is required",
+              pattern: {
+                value: /(^(\+88|0088)?(01){1}[3456789]{1}(\d){8})$/,
+                message: "invalid number",
+              },
+            })}
+            id="phoneNumber"
             className="w-full border rounded p-2 focus:border-green-400 focus:border focus:outline-none bg-white/50 "
           />
+          {errors.phoneNumber && (
+            <p className="text-red-500">
+              <small>{errors?.phoneNumber?.message}</small>
+            </p>
+          )}
         </div>
         <div>
           <label htmlFor="password" className="text-sm capitalize">
@@ -23,20 +43,21 @@ const LoginForm = () => {
           </label>
           <input
             type="password"
-            name="password"
+            {...register("password", { required: "Field is required" })}
             id="password"
             className="w-full border rounded p-2 focus:border-green-400 focus:border focus:outline-none bg-white/50 "
           />
+          {errors.password && <p>{errors?.message}</p>}
         </div>
-        <button
-          type="submit"
-          className="w-full text-center bg-green-400 p-2 text-white rounded font-medium  text-md hover:bg-green-500 hover:w-24 block mx-auto transition-all"
-        >
+        <button className="w-full text-center bg-green-400 p-2 text-white rounded font-medium  text-md hover:bg-green-500 hover:w-24 block mx-auto transition-all">
           Login
         </button>
       </form>
       <p className="mt-5 text-sm text-slate-400 text-center">
-        Not registered yet? <Link to="register">Register here</Link>
+        Not registered yet?
+        <Link className="text-black hover:text-green-500" to="register">
+          Register here
+        </Link>
       </p>
     </div>
   );
